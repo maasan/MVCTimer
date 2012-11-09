17e2d0c157e9026da63ed3343099a14ce1a215ad
Masato KONDO <kondo@isp.co.jp>
2012-11-09 17:16:45 +0900

    Initial Commit

---
+++ TimerTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TimerTest
+//
+//  Created by MK on 2012/11/09.
+//  Copyright (c) 2012年 MK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
